c38c5c9c10ca1e4ed5138619f8dd853f72d8e604
Fazi <sajawalk028@gmail.com>
2022-08-17 06:45:05 +1000

    changes

--- src/components/header/ProfileLogin.vue
+++ src/components/header/ProfileLogin.vue
@@ -2,7 +2,7 @@
   <section class="btns">
     <section v-if="!$store.state.auth.user">
       <button class="btn btn-transparent" @click="vendorModelShow">
-        Become Vendor v
+        Become Vendor
       </button>
       <button class="btn btn-filled" @click="loginModelShow">Login</button>
       <LoginModel :model="loginModel" />
