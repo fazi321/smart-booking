f59c705a5912c045f774cb062ebbf9e94b34b473
Fazi <socialsolution15@gmail.com>
2022-09-02 16:58:13 +0500

    upadted

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,14 +1,12 @@
 import axios from "axios";
-var https = require('https-browserify')
+import https from 'https-browserify';
 // import authHeader from "./authHeaders";
 /* eslint-disable */
 export default () =>
   axios.create({
     baseURL: "https://13.229.167.135:4000/api/v1/",
     withCredentials: false,
-    // httpsAgent: new https.Agent({  
-    //   rejectUnauthorized: false
-    // })
+    httpsAgent: new https.Agent({ rejectUnauthorized: false })
     // headers: authHeader(),
   });
-  https.globalAgent.options.rejectUnauthorized = false;
+
