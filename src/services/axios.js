7775d5cda1b21ea38014ab814d9d2d804a34bc41
Fazi <socialsolution15@gmail.com>
2022-09-02 15:09:29 +0500

    updated

--- src/services/axios.js
+++ src/services/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default () =>
   axios.create({
-    baseURL: "http://13.229.167.135:5000/api/v1/",
+    baseURL: "http://testingserver.tech:5000/api/v1/",
     withCredentials: false,
     // headers: authHeader(),
   });
